b2e28f7e3d19c96284207e43b2701d93c1b2aa54
Kanwarpreet Sethi <kpsethi@gmail.com>
2012-03-24 16:04:07 -0400

    First Activity: Status Update + supporting resources

---
+++ src/com/kanwarpreetsethi/yamba/StatusActivity.java
@@ -0,0 +1,38 @@
+package com.kanwarpreetsethi.yamba;
+
+import winterwell.jtwitter.Twitter;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class StatusActivity extends Activity implements OnClickListener{
+	
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.status);
+        
+        editText = (EditText) findViewById(R.id.statusText);
+        updateButton = (Button) findViewById(R.id.buttonUpdate);
+        
+        updateButton.setOnClickListener(this);
+        
+        twitter = new Twitter("student", "password");
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+    }
+
+    public void onClick(View v) {
+    	twitter.setStatus(editText.getText().toString());
+    	Log.d(TAG, "onClicked");
+    }
+}
